6bc74743fa641b9c5e067e88c733d329825475c6
Bartholomew anderson corigen <howtoedittv@gmail.com>
2026-03-22 17:02:58 +0200

    Refactor idle watcher and key tracking functions

--- src/shells/sav.fish
+++ src/shells/sav.fish
@@ -1,23 +1,29 @@
 #!/usr/bin/env fish
-# Run wallsav after 90s of inactivity, stops with a single keypress
 
 set IDLE_FILE /tmp/.last_command_time.$fish_pid
 date +%s >$IDLE_FILE
 
-# Update timer on every command
-function update_idle_file --on-event fish_prompt
+function update_idle_file
     date +%s >$IDLE_FILE
 end
 
-while true
-    sleep 1
-    set last (cat $IDLE_FILE)
-    set now (date +%s)
-    set idle (math $now - $last)
-    if test $idle -ge 45
-        # Launch wallsav in foreground, let it handle keypresses
-        ~/.local/bin/sav
-        # Reset idle timer after it finishes
-        date +%s >$IDLE_FILE
+function idle_watcher
+    while true
+        sleep 1
+        set last (cat $IDLE_FILE)
+        set now (date +%s)
+        set idle (math $now - $last)
+        if test $idle -ge 45
+            # Run your program in foreground
+            ~/.local/bin/sav
+            # Reset timer after it finishes
+            update_idle_file
+        end
     end
+end
+
+idle_watcher &
+
+function track_keys --on-event fish_key
+    update_idle_file
 end
